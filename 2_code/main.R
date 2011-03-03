e5f12497b3ad664801a1299049d6ab934687a5b4
ramnathv <ramnath.vaidya@gmail.com>
2011-03-03 18:40:57 -0500

    cleaned up the code

--- 2_code/main.R
+++ 2_code/main.R
@@ -5,23 +5,26 @@
 library(maptools);
 library(RColorBrewer);
 library(ggplot2);
+library(gridExtra);
 gpclibPermit();
+source('lib/utilities.R')
 
 ## LOAD MAP DATA (Source: http://goo.gl/bCUEF)
 
 shpfile = '1_data/maps/Precincts.shp';
 poly    = readShapeSpatial(shpfile);
 
-poly@data$id1 = with(poly@data, 
-                     paste(WARD, '-', PRECINCT));  # create id based on ward-precinct combo
-map           = fortify(poly, region = 'id1');     # fortify spatial polygon to dataframe
+poly@data$id  = with(poly@data, paste(WARD, '-', PRECINCT)); # create id =  ward-precinct
+map           = fortify(poly, region = 'id');                # fortify shp to data frame
 
 # LOAD ELECTION DATA (Source: http://goo.gl/HYwdV)
+# 
 
 df0     = read.csv('1_data/precincts.csv');
-df0$id2 = with(df0, paste(ward, '-', precinct));   # create id based on ward-precinct combo
-df0     = df0[,-grep('count', names(df0))];        # remove counts
-df0 = subset(df0, total_votes > 0)                 # remove precincts with zero total votes
+df0$id  = with(df0, paste(ward, '-', precinct));   # create id = ward-precinct
+df0     = df0[,-grep('count', names(df0))];        # remove vote counts
+# df0     = subset(df0, total_votes > 0)             # remove precincts with zero votes
+names(df0) = sub('_pct', '', names(df0));          # remove pct from names
 
 
 ## RECREATE PLOT IN CHICAGO TRIBUNE: http://goo.gl/ve65F
@@ -29,93 +32,85 @@ df0 = subset(df0, total_votes > 0)                 # remove precincts with zero 
 
 df = df0
 
-candidates = sub('_pct', '', names(df)[4:9])            # find list of candidates
-df$winner  = candidates[apply(df[,4:9], 1, which.max)]; # find winner of each election
-df$winpct  = apply(df[,4:9], 1, max);                   # find pct of votes secured by winner
-df$winint  = cut(df$winpct, c(0, 50, 75, 100));         # bin pct votes into intervals
+candidates = names(df)[4:9]                                  # find list of candidates
+df$winner  = candidates[apply(df[,4:9], 1, which.max)];      # find winner of each election
+df$winpct  = apply(df[,4:9], 1, max);                        # find pct of votes secured by winner
+df$winint  = cut(df$winpct, c(0, 50, 75, 100), labels = F,   # bin  pct winner votes
+                 include.lowest = T);   
+df$colpal  = sapply(df$winner, switch,                       # set palette for each winner
+                    'braun'    = 'PuRd', 
+                    'chico'    = 'Greens',
+                    'delvalle' = 'Oranges', 
+                    'emanuel'  = 'Purples');
+df = ddply(df, .(id), transform,                             # choose fill colors from colpal
+        fillcol = brewer.pal(5, colpal)[winint + 2]);     
+	
+## MERGE MAP WITH DATA AND PLOT
 
-# set winint to numeric values 3, 4 and 5 to use with brewer.pal
-df$win_paln = df$winint
-levels(df$win_paln) = 3:5
-df$win_paln = as.numeric(as.character(df$win_paln))
-
-# create color palette for each winner
-
-df$colpal = as.factor(df$winner);
-levels(df$colpal) = c('braun'    = 'PuRd',      'chico' = 'Greens', 
-                      'delvalle' = 'Oranges', 'emanuel' = 'Purples');
-df$colpal = as.character(df$colpal);
-
-# choose fillcolor for each ward-precinct based on winner and winpct
-
-choose_fill = function(win_paln, colpal){brewer.pal(5, colpal)[win_paln]};
-df$fillcol  = mdply(df[,14:15], choose_fill)$V1;
-
-## add winner and winner percentage to the data
-## fill color based on winner and winner percentage
-
-  	
-map.data  = merge(map, df, by.x = 'id', by.y = 'id2');
-map.data  = map.data[order(map.data$order),];
-
-source('~/Desktop/R Projects/Code Snippets/ggplot_themes.R');
-   
-p0 = ggplot(map.data, aes(long, lat, group = group)) +
-     geom_polygon(aes(fill = fillcol)) +
-     scale_fill_identity(name = 'Pct Votes for Winner', breaks = f1) +
-     theme_map() +
-     opts(legend.position = c(0.25, 0.35), 
-          title = 'Chicago Mayoral Elections 2011');
-
-
-print(p0);
-
-## function to sort colors sourced from http://goo.gl/3lg2
-sort.colours <- function(col) {
-  require(colorspace)
-  c.rgb = col2rgb(col)
-  c.RGB = RGB(t(c.rgb) %*% diag(rep(1/255, 3)))
-  c.HSV = as(c.RGB, "HSV")@coords
-  order(c.HSV[, 1], c.HSV[, 2], c.HSV[, 3])
-}
-
+map.data  = merge(map, df, by = 'id');                       # merge map with data
+map.data  = map.data[order(map.data$order),];                # maintain polygon order
+          
+# sort legend labels and create legend key
+          
+lcolors = unique(as.character(df$fillcol));
+lcolors = lcolors[sort.colours(lcolors)]
+lcolors = lcolors[c(2,1,3:length(lcolors))];
 labs = c('< 50 del Valle', 
          '> 50', 
          '< 50 Chico', 
-         '50 - 75', 
+         '> 50', 
          '> 75', 
          '< 50 Emanuel', 
-         '50 - 75', 
+         '> 50', 
          '> 75', 
-         '> 50 Braun')
-## RECREATE OFFENSIVE POLITICS PLOTS USING FACET
+         '> 50 Braun');
 
-# merge fortified map with data on elections
-map.data2 = merge(map, df0, by.x = 'id', by.y = 'id2');
+p0 = ggplot(map.data, aes(long, lat, group = group)) +
+     geom_polygon(aes(fill = fillcol)) +
+     scale_fill_identity(name = 'Pct Votes for Winner', breaks = lcolors, labels = labs) +
+     theme_map() +
+     opts(legend.position = c(0.25, 0.35), 
+     title = 'Chicago Mayoral Elections 2011');
+         
 
-# melt the data and clean up the variable names
-dfm = melt(map.data2, measure.vars = grep('pct', names(map.data2)))
-names(dfm)[names(dfm) == 'variable'] = 'candidate';
-names(dfm)[names(dfm) == 'value']    = 'vote_pct';
-dfm$candidate = with(dfm, sub('_pct', '', candidate));
+## RECREATE OFFENSIVE POLITICS PLOTS
 
-# order the dataframe so that ggplot can handle it correctly
-dfm = dfm[order(dfm$order),];
+dfm = melt(map.data, measure.vars = candidates, var = 'candidate')  # melt data by candidate
+dfm = dfm[order(dfm$order),];                                       # maintain polygon order
 
-
-# create a function that generates a plot for a specified candidate
-plotCandidate <- function(cand){
+# FUNCTION TO GENERATE PLOT FOR A SPECIFIC CANDIDATE
+plotCandidate <- function(cand, colPal = 'Blues'){
 	
 	df = subset(dfm, candidate == cand);
 	df = df[order(df$order),]
 	p0 = ggplot(df, aes(long, lat, group = group)) +
-	     geom_polygon(aes(fill = cut(vote_pct, seq(0, 100, by = 20)))) +
-	     scale_fill_brewer(name = paste(cand, '(% Votes)'),pal = 'Blues') +
-	     opts(title = 'Chicago Mayoral Race') +
-	     theme_map();
+	     geom_polygon(aes(fill = cut(value, seq(0, 100, by = 20), include.lowest = T))) +
+	     scale_fill_brewer(name = paste(capwords(cand), '(% Votes)'),pal = colPal) +
+	     theme_map() + 
+       opts(legend.position = c(0.25, 0.35), 
+       title = 'Chicago Mayoral Elections 2011');
 	return(p0)
 	
 }
+
+plotEmanuel = plotCandidate('emanuel');
+print(plotEmanuel);
+
+candidate_plots = llply(candidates, plotCandidate, .progress = 'text');
+saveCandidateplots <- function(candidate){
+  p = plotCandidate(candidate);
+  f = file.path('3_figures', paste(candidate, '.pdf', sep = ''));
+  savePlotAA(p, f);
+}
+  
+l_ply(candidates, saveCandidateplots);
+  
+  
+
+
+ 
+plotEmanuel = plotCandidate('emanuel');
+print(plotEmanuel);
 
 ## NEXT STEPS
 ## 1. clean up the code
