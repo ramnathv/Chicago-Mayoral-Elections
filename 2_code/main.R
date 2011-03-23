3a947abc4ca92a1c761172c6c50f03f878666ff0
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2011-03-23 13:44:41 -0400

    modfiying files

--- 2_code/main.R
+++ 2_code/main.R
@@ -1,6 +1,10 @@
-## Choropleth Map of Chicago Mayoral Elections 2011
+## CHOROPLETH MAP OF CHICAGO MAYORAL ELECTIONS 2011
+#  Recently, the Chicago Tribune (Source: http://goo.gl/ve65F) presented a nice 
+#  infographic that visualizes the percentage of votes secured by the winner in each
+#  precinct. This is an attempt to recreate this infographic using R and ggplot2.
 
-## LOAD LIBRARIES
+
+## LOAD LIBRARIES AND CUSTOM FUNCTIONS
 
 library(maptools);
 library(RColorBrewer);
@@ -9,115 +13,99 @@ library(gridExtra);
 gpclibPermit();
 source('lib/utilities.R')
 
-## LOAD MAP DATA (Source: http://goo.gl/bCUEF)
+## STEP 0: Load Data
 
+# load map data (Source: http://goo.gl/bCUEF)
 shpfile = '1_data/maps/Precincts.shp';
-poly    = readShapeSpatial(shpfile);
+poly    = readShapeSpatial(shpfile);          
 
-poly@data$id  = with(poly@data, paste(WARD, '-', PRECINCT)); # create id =  ward-precinct
-map           = fortify(poly, region = 'id');                # fortify shp to data frame
+# load election results data (Source: http://goo.gl/HYwdV)
+df        = read.csv('1_data/precincts.csv');
 
-# LOAD ELECTION DATA (Source: http://goo.gl/HYwdV)
-# 
+## STEP 1: Clean up and Process Data
 
-df0     = read.csv('1_data/precincts.csv');
-df0$id  = with(df0, paste(ward, '-', precinct));   # create id = ward-precinct
-df0     = df0[,-grep('count', names(df0))];        # remove vote counts
-# df0     = subset(df0, total_votes > 0)             # remove precincts with zero votes
-names(df0) = sub('_pct', '', names(df0));          # remove pct from names
+# remove vote counts and 'pct' from names
+df        = df[,-grep('count', names(df))];         
+names(df) = sub('_pct', '', names(df));             
 
 
-## RECREATE PLOT IN CHICAGO TRIBUNE: http://goo.gl/ve65F
-## TODO: fix legend to match that in the article
+# find winner and their pct of votes secured for each precinct
+candidates = names(df)[4:9];                                
+df$winner  = candidates[apply(df[, candidates], 1, which.max)];    
+df$winpct  = apply(df[, candidates], 1, max);                     
+ 
+# assign a color palette to each winner
+df$colpal  = sapply(df$winner, switch, 'braun' = 'PuRd', 'chico' = 'Greens',
+                    'delvalle' = 'Oranges', 'emanuel'  = 'Purples');
 
-df = df0
+# bin the pct of votes secured by winner into discrete intervals
+df$winint  = cut(df$winpct, c(0, 50, 75, 100), labels = F, include.lowest = T);  
 
-candidates = names(df)[4:9]                                  # find list of candidates
-df$winner  = candidates[apply(df[,4:9], 1, which.max)];      # find winner of each election
-df$winpct  = apply(df[,4:9], 1, max);                        # find pct of votes secured by winner
-df$winint  = cut(df$winpct, c(0, 50, 75, 100), labels = F,   # bin  pct winner votes
-                 include.lowest = T);   
-df$colpal  = sapply(df$winner, switch,                       # set palette for each winner
-                    'braun'    = 'PuRd', 
-                    'chico'    = 'Greens',
-                    'delvalle' = 'Oranges', 
-                    'emanuel'  = 'Purples');
-df = ddply(df, .(id), transform,                             # choose fill colors from colpal
-        fillcol = brewer.pal(5, colpal)[winint + 2]);     
+# choose color for each precinct based on palette and bin
+df = adply(df, 1, transform, fillcol = brewer.pal(5, colpal)[winint + 2]);     
 	
-## MERGE MAP WITH DATA AND PLOT
+## STEP 2: Merge shapefile with election results data by
 
-map.data  = merge(map, df, by = 'id');                       # merge map with data
-map.data  = map.data[order(map.data$order),];                # maintain polygon order
-          
-# sort legend labels and create legend key
-          
-lcolors = unique(as.character(df$fillcol));
-lcolors = lcolors[sort.colours(lcolors)]
-lcolors = lcolors[c(2,1,3:length(lcolors))];
-labs = c('< 50 del Valle', 
-         '> 50', 
-         '< 50 Chico', 
-         '> 50', 
-         '> 75', 
-         '< 50 Emanuel', 
-         '> 50', 
-         '> 75', 
-         '> 50 Braun');
+# create id = ward-precinct to match the shapefile with data
+poly@data$id  = with(poly@data, paste(WARD, '-', PRECINCT));
+df$id         = with(df, paste(ward, '-', precinct)); 
 
+# fortify shape file to data frame and merge with data
+map       = fortify(poly, region = 'id');        
+map.data  = merge(map, df, by = 'id');                      
+map.data  = map.data[order(map.data$order),];    
+
+## STEP 3a: Create the plot in Chicago Tribute!!!           
+
+# identify legend colors and sort them by their rgb values
+leg.colors = unique(as.character(df$fillcol));
+leg.colors = leg.colors[sort.colours(leg.colors)];
+leg.colors = leg.colors[c(2,1,3:length(leg.colors))];
+
+# assign meaningful labels to each color
+leg.labels = c('< 50 del Valle', '> 50', '< 50 Chico', '> 50', '> 75', 
+               '< 50 Emanuel', '> 50', '> 75', '> 50 Braun');
+
+
+# draw the plot
 p0 = ggplot(map.data, aes(long, lat, group = group)) +
      geom_polygon(aes(fill = fillcol)) +
-     scale_fill_identity(name = 'Pct Votes for Winner', breaks = lcolors, labels = labs) +
+     scale_fill_identity(name   = 'Pct Votes for Winner', 
+						 breaks = leg.colors, 
+                         labels = leg.labels) +
      theme_map() +
      opts(legend.position = c(0.25, 0.35), 
-     title = 'Chicago Mayoral Elections 2011');
+          title = 'Chicago Mayoral Elections 2011');
+
+# save it as png
+ggsave('3_figures/winner5.png', p0, width = 7, height = 7);
          
 
-## RECREATE OFFENSIVE POLITICS PLOTS
+## STEP 3b. Create the plots in the offensive Politics Blog
 
-dfm = melt(map.data, measure.vars = candidates, var = 'candidate')  # melt data by candidate
-dfm = dfm[order(dfm$order),];                                       # maintain polygon order
-
-# FUNCTION TO GENERATE PLOT FOR A SPECIFIC CANDIDATE
-plotCandidate <- function(cand, colPal = 'Blues'){
+# function to creaet a plot by candidate
+plotChoro <- function(mapdata, cand, colPal = 'PuRd'){
 	
-	df = subset(dfm, candidate == cand);
-	df = df[order(df$order),]
-	p0 = ggplot(df, aes(long, lat, group = group)) +
-	     geom_polygon(aes(fill = cut(value, seq(0, 100, by = 20), include.lowest = T))) +
-	     scale_fill_brewer(name = paste(capwords(cand), '(% Votes)'),pal = colPal) +
+	# bin pct of votes secured by candidate into discrete intervals
+	mapdata$bin = cut(mapdata[,cand], seq(0, 100, by = 20), include.lowest = T);
+	
+	p0 = ggplot(mapdata, aes(long, lat, group = group)) +
+	     geom_polygon(aes(fill = bin)) +
+	     scale_fill_brewer(name = paste(capwords(cand), '(% Votes)'), pal = colPal) +
 	     theme_map() + 
-       opts(legend.position = c(0.25, 0.35), 
-       title = 'Chicago Mayoral Elections 2011');
+         opts(legend.position = c(0.25, 0.35), 
+              title = 'Chicago Mayoral Elections 2011');
 	return(p0)
 	
 }
 
-plotEmanuel = plotCandidate('emanuel');
-print(plotEmanuel);
-
-candidate_plots = llply(candidates, plotCandidate, .progress = 'text');
-saveCandidateplots <- function(candidate){
+savePNG <- function(candidate){
   p = plotCandidate(candidate);
-  f = file.path('3_figures', paste(candidate, '.pdf', sep = ''));
-  savePlotAA(p, f);
+  f = file.path('3_figures', paste(candidate, '.png', sep = ''));
+  ggsave(f, p, width = 7, height = 7)
 }
   
-l_ply(candidates, saveCandidateplots);
-  
-  
-
-
- 
-plotEmanuel = plotCandidate('emanuel');
-print(plotEmanuel);
-
-## NEXT STEPS
-## 1. clean up the code
-## 2. create a blog entry and post it
-## 3. figure out how to overlay on top of google maps
-## 4. 
-
+l_ply(candidates, savePNG);
 
 
 
--- lib/utilities.R
+++ lib/utilities.R
@@ -1,3 +1,9 @@
+## LOAD LIBRARIES
+
+library(maptools); gpclibPermit();
+library(ggplot2); library(RColorBrewer);
+library(gdata);
+
 ## PLAIN THEME FOR MAP
 ## Source: ggplot mailing list
 
@@ -23,14 +29,14 @@ theme_map = function(size = 12) {
 
 			 ## PANEL
              panel.background  = theme_blank(), 
-             panel.border      = theme_blank(), 
-             panel.grid.major  = theme_blank(), 
-             panel.grid.minor  = theme_blank(), 
+             panel.border      = theme_rect(fill = NA, colour = "grey50"), 
+             panel.grid.major  = theme_line(colour = 'grey90', size = 0.2),
+             panel.grid.minor  = theme_line(colour = 'grey98', size = 0.5), 
              panel.margin      = unit(0, "lines"), 
 
 			 ## PLOT
              plot.background   = theme_blank(), 
-			 plot.margin       = unit(c(1, 1, 0.5, 0.5), "lines"), 
+			       plot.margin       = unit(c(1, 1, 0.5, 0.5), "lines"), 
              plot.title        = theme_text(size = size*1.2), 
              strip.background  = theme_rect(fill = "grey90", colour = "grey50"), 
              strip.text.x      = theme_text(size = size*0.8), 
@@ -61,6 +67,70 @@ capwords <- function(s, strict = FALSE) {
     sapply(strsplit(s, split = " "), cap, USE.NAMES = !is.null(names(s)))
 }
 
+# FUNCTION TO PLOT A CHOROPLETH MAP
+plotChoropleth <- function(.poly, id1, field, 
+   .df = .poly@data, id2 = id1, fill = NA, title = "", legtitle = "Values",
+   colpal = 'PuRd', fixed = F, ...){
+		
+	 # load required libraries
+   library(maptools); library(spatial); library(RColorBrewer);
+   library(classInt); library(ggplot2); gpclibPermit();
+	
+	 # extract relevant columns of .df if fill is unspecified
+   if(all(is.na(fill))) {.df = .df[, c(unlist(id2), field)]};	
+   
+   # create id to match shapefile with data
+   id1 = llply(id1, as.name); id2 = llply(id2, as.name);
+  .poly@data$id  = with(.poly@data, do.call('paste', c(id1, sep = "-")));
+  .df$id         = with(.df,        do.call('paste', c(id2, sep = "-"))); 
+	 
+	 # fortify shape file to data frame and merge with data
+   map       = fortify(.poly, region = 'id');       
+   map.data  = merge(map, .df, by = 'id');
+   map.data  = map.data[order(map.data$order),];
+	
+	 if (!is.na(fill)){
+     
+    # if fill color is specified, use it for the plot 
+		p1 = ggplot(map.data, aes(long, lat, group = group)) +  
+		     geom_polygon(aes_string(fill = fill)) + 
+		     scale_fill_identity(name = legtitle) +
+		     theme_map();
+		
+ 	} else {
+     
+   # if fill color is not specified, melt the data frame
+	 map.data = melt(map.data, measure.vars = field);                      
+   map.data = map.data[order(map.data$order),];
+    
+   # figure out binning intervals and fill colors
+   if (fixed == T){
+       # treatment of fixed breaks separately is necessitated by
+       # a possible bug in classIntervals
+       intvl  = cut(x = map.data$value, ...);
+       colors = brewer.pal(length(levels(intvl)), colpal);
+       fill   = colors[intvl];
+       legkey = levels(intvl);
+         
+    } else {
+      
+       intvl   = classIntervals(var = map.data$value, ...);
+       colors  = brewer.pal(length(intvl$brks) - 1, colpal);
+       fill    = findColours(intvl, colors, digits = 4);
+       legkey  = names(attr(fill, "table"));
+    }
+		map.data$fill = fill;
+		p1 = ggplot(map.data, aes(long, lat, group = group)) + 
+		     geom_polygon(aes(fill = fill)) +
+		     scale_fill_identity(name = legtitle, breaks = colors, labels = legkey) +
+		     facet_wrap(~ variable) +
+		     theme_map();
+	}
+	
+	return(p1 + opts(title = title));
+	    
+}
+
 
 ## FUNCTION TO ADD FOOTNOTE TO A PLOT
 ## Author: Mark Heckmann (http://goo.gl/2H69K)
@@ -87,4 +157,10 @@ savePlotAA <- function(p, filename = 'Rplots', width = 7, height = 7, source = N
     print(p);
     makeFootnote(source);
     dev.off()
+}
+
+savePDF <- function(candidate){
+  p = plotCandidate(candidate);
+  f = file.path('3_figures', paste(candidate, '.pdf', sep = ''));
+  savePlotAA(p, f);
 }
